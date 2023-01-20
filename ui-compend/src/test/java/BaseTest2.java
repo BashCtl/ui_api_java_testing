2695abd398ad1a9755b1062bc38a67a8974c55f4
space <ki14.skakun@gmail.com>
2023-01-20 12:39:58 +0200

    set up circleci configs

---
+++ ui-compend/src/test/java/BaseTest2.java
@@ -0,0 +1,44 @@
+
+import com.ui.configs.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.*;
+
+public class BaseTest2 {
+    private WebDriver driver;
+
+    @Parameters("browser")
+    @BeforeClass(alwaysRun = true)
+    public void beforeClass(@Optional("chrome") String browser) {
+        System.setProperty("browser", browser);
+        driver = new WebDriverFactory().getDriver();
+    }
+
+    @AfterClass
+    public void afterClass(){
+        driver.quit();
+    }
+
+    @Test
+    public void test() {
+
+        driver.get("https://compendiumdev.co.uk/");
+    }
+
+    @Test
+    public void test2() {
+
+        driver.get("https://compendiumdev.co.uk/");
+    }
+
+    @Test
+    public void tes3() {
+
+        driver.get("https://compendiumdev.co.uk/");
+    }
+
+    @Test
+    public void test4() {
+
+        driver.get("https://compendiumdev.co.uk/");
+    }
+}
