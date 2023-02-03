8833d563bdd098ab281632f88620dcbc1b60efb2
space <ki14.skakun@gmail.com>
2023-02-03 16:29:55 +0200

    Add fake registration tests

--- ui-compend/src/test/java/tests/RegisterTest.java
+++ ui-compend/src/test/java/tests/RegisterTest.java
@@ -7,9 +7,24 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import steps.RegisterSteps;
 
-public class RegisterTest extends BaseTest{
+public class RegisterTest extends BaseTest {
     private RegisterSteps registerSteps;
     private String email;
+    private String firstName;
+    private String lastName;
+    private int phoneCode;
+
+    private String phoneNumber;
+
+    private String companyName;
+    private String firstAddress;
+    private String secondAddress;
+    private String city;
+    private String state;
+    private String postCode;
+    private String country;
+    private String mobilePhone;
+    private String password;
 
     @BeforeMethod(alwaysRun = true)
     @Override
@@ -17,11 +32,23 @@ public class RegisterTest extends BaseTest{
         super.beforeMethod(context);
         registerSteps = new RegisterSteps(driver);
         email = fakeValuesService.bothify("????###@mail.test");
+        firstName = faker.name().firstName();
+        lastName = faker.name().lastName();
+        phoneCode = user.getDialCode();
+        phoneNumber = faker.phoneNumber().subscriberNumber();
+        companyName = faker.company().name();
+        firstAddress = faker.address().streetAddress();
+        secondAddress = faker.address().secondaryAddress();
+        city = faker.address().city();
+        postCode = faker.address().zipCode();
+        mobilePhone = faker.phoneNumber().cellPhone();
+        password = faker.internet().password();
+
     }
 
     @Description("User Registration Without Checking Captcha Checkbox.")
     @Test
-    public void registerUserWithoutRecaptcha(){
+    public void registerUserWithoutRecaptcha() {
         registerSteps.openPage()
                 .enterFirstName(user.getFirstName())
                 .enterLastName(user.getLastName())
@@ -38,6 +65,255 @@ public class RegisterTest extends BaseTest{
                 .enterMobilePhone(user.getMobilePhone())
                 .enterPassword(user.getPassword())
                 .confirmPassword(user.getPassword())
+//                .checkRecaptcha()
+                .clickRegisterBtn();
+//        String error = registerSteps.getErrorText();
+//        Assert.assertEquals(error, "Please complete the captcha and try again.");
+
+    }
+
+    @Test
+    public void registerUserWithoutRecaptchaFakeForAmount1() {
+        registerSteps.openPage()
+                .enterFirstName(firstName)
+                .enterLastName(lastName)
+                .enterEmail(email)
+                .selectDialCode(user.getDialCode())
+                .enterPhoneNumber(phoneNumber)
+                .selectCountry(billingAddress.getCountry())
+                .selectState(billingAddress.getState())
+                .enterCompanyName(companyName)
+                .enterFirstAddress(firstAddress)
+                .enterSecondAddress(secondAddress)
+                .enterCity(city)
+                .enterPostalCode(postCode)
+                .enterMobilePhone(mobilePhone)
+                .enterPassword(password)
+                .confirmPassword(password)
+//                .checkRecaptcha()
+                .clickRegisterBtn();
+//        String error = registerSteps.getErrorText();
+//        Assert.assertEquals(error, "Please complete the captcha and try again.");
+
+    }
+    @Test
+    public void registerUserWithoutRecaptchaFakeForAmount2() {
+        registerSteps.openPage()
+                .enterFirstName(firstName)
+                .enterLastName(lastName)
+                .enterEmail(email)
+                .selectDialCode(user.getDialCode())
+                .enterPhoneNumber(phoneNumber)
+                .selectCountry(billingAddress.getCountry())
+                .selectState(billingAddress.getState())
+                .enterCompanyName(companyName)
+                .enterFirstAddress(firstAddress)
+                .enterSecondAddress(secondAddress)
+                .enterCity(city)
+                .enterPostalCode(postCode)
+                .enterMobilePhone(mobilePhone)
+                .enterPassword(password)
+                .confirmPassword(password)
+//                .checkRecaptcha()
+                .clickRegisterBtn();
+//        String error = registerSteps.getErrorText();
+//        Assert.assertEquals(error, "Please complete the captcha and try again.");
+
+    }
+
+    @Test
+    public void registerUserWithoutRecaptchaFakeForAmount3() {
+        registerSteps.openPage()
+                .enterFirstName(firstName)
+                .enterLastName(lastName)
+                .enterEmail(email)
+                .selectDialCode(user.getDialCode())
+                .enterPhoneNumber(phoneNumber)
+                .selectCountry(billingAddress.getCountry())
+                .selectState(billingAddress.getState())
+                .enterCompanyName(companyName)
+                .enterFirstAddress(firstAddress)
+                .enterSecondAddress(secondAddress)
+                .enterCity(city)
+                .enterPostalCode(postCode)
+                .enterMobilePhone(mobilePhone)
+                .enterPassword(password)
+                .confirmPassword(password)
+//                .checkRecaptcha()
+                .clickRegisterBtn();
+//        String error = registerSteps.getErrorText();
+//        Assert.assertEquals(error, "Please complete the captcha and try again.");
+
+    }
+
+    @Test
+    public void registerUserWithoutRecaptchaFakeForAmount4() {
+        registerSteps.openPage()
+                .enterFirstName(firstName)
+                .enterLastName(lastName)
+                .enterEmail(email)
+                .selectDialCode(user.getDialCode())
+                .enterPhoneNumber(phoneNumber)
+                .selectCountry(billingAddress.getCountry())
+                .selectState(billingAddress.getState())
+                .enterCompanyName(companyName)
+                .enterFirstAddress(firstAddress)
+                .enterSecondAddress(secondAddress)
+                .enterCity(city)
+                .enterPostalCode(postCode)
+                .enterMobilePhone(mobilePhone)
+                .enterPassword(password)
+                .confirmPassword(password)
+//                .checkRecaptcha()
+                .clickRegisterBtn();
+//        String error = registerSteps.getErrorText();
+//        Assert.assertEquals(error, "Please complete the captcha and try again.");
+
+    }
+
+    @Test
+    public void registerUserWithoutRecaptchaFakeForAmount5() {
+        registerSteps.openPage()
+                .enterFirstName(firstName)
+                .enterLastName(lastName)
+                .enterEmail(email)
+                .selectDialCode(user.getDialCode())
+                .enterPhoneNumber(phoneNumber)
+                .selectCountry(billingAddress.getCountry())
+                .selectState(billingAddress.getState())
+                .enterCompanyName(companyName)
+                .enterFirstAddress(firstAddress)
+                .enterSecondAddress(secondAddress)
+                .enterCity(city)
+                .enterPostalCode(postCode)
+                .enterMobilePhone(mobilePhone)
+                .enterPassword(password)
+                .confirmPassword(password)
+//                .checkRecaptcha()
+                .clickRegisterBtn();
+//        String error = registerSteps.getErrorText();
+//        Assert.assertEquals(error, "Please complete the captcha and try again.");
+
+    }
+
+    @Test
+    public void registerUserWithoutRecaptchaFakeForAmount6() {
+        registerSteps.openPage()
+                .enterFirstName(firstName)
+                .enterLastName(lastName)
+                .enterEmail(email)
+                .selectDialCode(user.getDialCode())
+                .enterPhoneNumber(phoneNumber)
+                .selectCountry(billingAddress.getCountry())
+                .selectState(billingAddress.getState())
+                .enterCompanyName(companyName)
+                .enterFirstAddress(firstAddress)
+                .enterSecondAddress(secondAddress)
+                .enterCity(city)
+                .enterPostalCode(postCode)
+                .enterMobilePhone(mobilePhone)
+                .enterPassword(password)
+                .confirmPassword(password)
+//                .checkRecaptcha()
+                .clickRegisterBtn();
+//        String error = registerSteps.getErrorText();
+//        Assert.assertEquals(error, "Please complete the captcha and try again.");
+
+    }
+
+    @Test
+    public void registerUserWithoutRecaptchaFakeForAmount7() {
+        registerSteps.openPage()
+                .enterFirstName(firstName)
+                .enterLastName(lastName)
+                .enterEmail(email)
+                .selectDialCode(user.getDialCode())
+                .enterPhoneNumber(phoneNumber)
+                .selectCountry(billingAddress.getCountry())
+                .selectState(billingAddress.getState())
+                .enterCompanyName(companyName)
+                .enterFirstAddress(firstAddress)
+                .enterSecondAddress(secondAddress)
+                .enterCity(city)
+                .enterPostalCode(postCode)
+                .enterMobilePhone(mobilePhone)
+                .enterPassword(password)
+                .confirmPassword(password)
+//                .checkRecaptcha()
+                .clickRegisterBtn();
+//        String error = registerSteps.getErrorText();
+//        Assert.assertEquals(error, "Please complete the captcha and try again.");
+
+    }
+
+    @Test
+    public void registerUserWithoutRecaptchaFakeForAmount8() {
+        registerSteps.openPage()
+                .enterFirstName(firstName)
+                .enterLastName(lastName)
+                .enterEmail(email)
+                .selectDialCode(user.getDialCode())
+                .enterPhoneNumber(phoneNumber)
+                .selectCountry(billingAddress.getCountry())
+                .selectState(billingAddress.getState())
+                .enterCompanyName(companyName)
+                .enterFirstAddress(firstAddress)
+                .enterSecondAddress(secondAddress)
+                .enterCity(city)
+                .enterPostalCode(postCode)
+                .enterMobilePhone(mobilePhone)
+                .enterPassword(password)
+                .confirmPassword(password)
+//                .checkRecaptcha()
+                .clickRegisterBtn();
+//        String error = registerSteps.getErrorText();
+//        Assert.assertEquals(error, "Please complete the captcha and try again.");
+
+    }
+
+    @Test
+    public void registerUserWithoutRecaptchaFakeForAmount9() {
+        registerSteps.openPage()
+                .enterFirstName(firstName)
+                .enterLastName(lastName)
+                .enterEmail(email)
+                .selectDialCode(user.getDialCode())
+                .enterPhoneNumber(phoneNumber)
+                .selectCountry(billingAddress.getCountry())
+                .selectState(billingAddress.getState())
+                .enterCompanyName(companyName)
+                .enterFirstAddress(firstAddress)
+                .enterSecondAddress(secondAddress)
+                .enterCity(city)
+                .enterPostalCode(postCode)
+                .enterMobilePhone(mobilePhone)
+                .enterPassword(password)
+                .confirmPassword(password)
+//                .checkRecaptcha()
+                .clickRegisterBtn();
+//        String error = registerSteps.getErrorText();
+//        Assert.assertEquals(error, "Please complete the captcha and try again.");
+
+    }
+
+    @Test
+    public void registerUserWithoutRecaptchaFakeForAmount10() {
+        registerSteps.openPage()
+                .enterFirstName(firstName)
+                .enterLastName(lastName)
+                .enterEmail(email)
+                .selectDialCode(user.getDialCode())
+                .enterPhoneNumber(phoneNumber)
+                .selectCountry(billingAddress.getCountry())
+                .selectState(billingAddress.getState())
+                .enterCompanyName(companyName)
+                .enterFirstAddress(firstAddress)
+                .enterSecondAddress(secondAddress)
+                .enterCity(city)
+                .enterPostalCode(postCode)
+                .enterMobilePhone(mobilePhone)
+                .enterPassword(password)
+                .confirmPassword(password)
 //                .checkRecaptcha()
                 .clickRegisterBtn();
 //        String error = registerSteps.getErrorText();
