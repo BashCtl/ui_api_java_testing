a2252e0803c2ffcee2b396f981bb8e459f2d1b18
space <ki14.skakun@gmail.com>
2023-02-04 11:27:35 +0200

    tests for total amount

--- ui-compend/src/test/java/tests/InstallationTest.java
+++ ui-compend/src/test/java/tests/InstallationTest.java
@@ -36,4 +36,184 @@ public class InstallationTest extends BaseTest {
         String price = installationSteps.getPriceByTitle(installation.getUpgradeTitle());
         assertEquals(price, installation.getUpgradePrice());
     }
+
+    @Test
+    public void verifyAppInstallationPrice1() {
+        String price = installationSteps.getPriceByTitle(installation.getAppInstallTitle());
+        assertEquals(price, installation.getAppInstallPrice());
+    }
+
+    @Test
+    public void verifyTroubleshootPrice1() {
+        String price = installationSteps.getPriceByTitle(installation.getTroubleshoot());
+        assertEquals(price, installation.getTroubleshootPrice());
+    }
+
+    @Test
+    public void verifyUpgradePrice1(){
+        String price = installationSteps.getPriceByTitle(installation.getUpgradeTitle());
+        assertEquals(price, installation.getUpgradePrice());
+    }
+
+    @Test
+    public void verifyAppInstallationPrice2() {
+        String price = installationSteps.getPriceByTitle(installation.getAppInstallTitle());
+        assertEquals(price, installation.getAppInstallPrice());
+    }
+
+    @Test
+    public void verifyTroubleshootPrice2() {
+        String price = installationSteps.getPriceByTitle(installation.getTroubleshoot());
+        assertEquals(price, installation.getTroubleshootPrice());
+    }
+
+    @Test
+    public void verifyUpgradePrice2(){
+        String price = installationSteps.getPriceByTitle(installation.getUpgradeTitle());
+        assertEquals(price, installation.getUpgradePrice());
+    }
+
+    @Test
+    public void verifyAppInstallationPrice3() {
+        String price = installationSteps.getPriceByTitle(installation.getAppInstallTitle());
+        assertEquals(price, installation.getAppInstallPrice());
+    }
+
+    @Test
+    public void verifyTroubleshootPrice3() {
+        String price = installationSteps.getPriceByTitle(installation.getTroubleshoot());
+        assertEquals(price, installation.getTroubleshootPrice());
+    }
+
+    @Test
+    public void verifyUpgradePrice3(){
+        String price = installationSteps.getPriceByTitle(installation.getUpgradeTitle());
+        assertEquals(price, installation.getUpgradePrice());
+    }
+
+    @Test
+    public void verifyAppInstallationPrice4() {
+        String price = installationSteps.getPriceByTitle(installation.getAppInstallTitle());
+        assertEquals(price, installation.getAppInstallPrice());
+    }
+
+    @Test
+    public void verifyTroubleshootPrice4() {
+        String price = installationSteps.getPriceByTitle(installation.getTroubleshoot());
+        assertEquals(price, installation.getTroubleshootPrice());
+    }
+
+    @Test
+    public void verifyUpgradePrice4(){
+        String price = installationSteps.getPriceByTitle(installation.getUpgradeTitle());
+        assertEquals(price, installation.getUpgradePrice());
+    }
+
+    @Test
+    public void verifyAppInstallationPrice5() {
+        String price = installationSteps.getPriceByTitle(installation.getAppInstallTitle());
+        assertEquals(price, installation.getAppInstallPrice());
+    }
+
+    @Test
+    public void verifyTroubleshootPrice5() {
+        String price = installationSteps.getPriceByTitle(installation.getTroubleshoot());
+        assertEquals(price, installation.getTroubleshootPrice());
+    }
+
+    @Test
+    public void verifyUpgradePrice5(){
+        String price = installationSteps.getPriceByTitle(installation.getUpgradeTitle());
+        assertEquals(price, installation.getUpgradePrice());
+    }
+
+    @Test
+    public void verifyAppInstallationPrice6() {
+        String price = installationSteps.getPriceByTitle(installation.getAppInstallTitle());
+        assertEquals(price, installation.getAppInstallPrice());
+    }
+
+    @Test
+    public void verifyTroubleshootPrice6() {
+        String price = installationSteps.getPriceByTitle(installation.getTroubleshoot());
+        assertEquals(price, installation.getTroubleshootPrice());
+    }
+
+    @Test
+    public void verifyUpgradePrice6(){
+        String price = installationSteps.getPriceByTitle(installation.getUpgradeTitle());
+        assertEquals(price, installation.getUpgradePrice());
+    }
+
+    @Test
+    public void verifyAppInstallationPrice7() {
+        String price = installationSteps.getPriceByTitle(installation.getAppInstallTitle());
+        assertEquals(price, installation.getAppInstallPrice());
+    }
+
+    @Test
+    public void verifyTroubleshootPrice7() {
+        String price = installationSteps.getPriceByTitle(installation.getTroubleshoot());
+        assertEquals(price, installation.getTroubleshootPrice());
+    }
+
+    @Test
+    public void verifyUpgradePrice7(){
+        String price = installationSteps.getPriceByTitle(installation.getUpgradeTitle());
+        assertEquals(price, installation.getUpgradePrice());
+    }
+
+    @Test
+    public void verifyAppInstallationPrice8() {
+        String price = installationSteps.getPriceByTitle(installation.getAppInstallTitle());
+        assertEquals(price, installation.getAppInstallPrice());
+    }
+
+    @Test
+    public void verifyTroubleshootPrice8() {
+        String price = installationSteps.getPriceByTitle(installation.getTroubleshoot());
+        assertEquals(price, installation.getTroubleshootPrice());
+    }
+
+    @Test
+    public void verifyUpgradePrice8(){
+        String price = installationSteps.getPriceByTitle(installation.getUpgradeTitle());
+        assertEquals(price, installation.getUpgradePrice());
+    }
+
+    @Test
+    public void verifyAppInstallationPrice9() {
+        String price = installationSteps.getPriceByTitle(installation.getAppInstallTitle());
+        assertEquals(price, installation.getAppInstallPrice());
+    }
+
+    @Test
+    public void verifyTroubleshootPrice9() {
+        String price = installationSteps.getPriceByTitle(installation.getTroubleshoot());
+        assertEquals(price, installation.getTroubleshootPrice());
+    }
+
+    @Test
+    public void verifyUpgradePrice9(){
+        String price = installationSteps.getPriceByTitle(installation.getUpgradeTitle());
+        assertEquals(price, installation.getUpgradePrice());
+    }
+
+    @Test
+    public void verifyAppInstallationPrice10() {
+        String price = installationSteps.getPriceByTitle(installation.getAppInstallTitle());
+        assertEquals(price, installation.getAppInstallPrice());
+    }
+
+    @Test
+    public void verifyTroubleshootPrice10() {
+        String price = installationSteps.getPriceByTitle(installation.getTroubleshoot());
+        assertEquals(price, installation.getTroubleshootPrice());
+    }
+
+    @Test
+    public void verifyUpgradePrice10(){
+        String price = installationSteps.getPriceByTitle(installation.getUpgradeTitle());
+        assertEquals(price, installation.getUpgradePrice());
+    }
 }
--- ui-compend/src/test/java/tests/MobileTest.java
+++ ui-compend/src/test/java/tests/MobileTest.java
@@ -30,4 +30,124 @@ public class MobileTest extends BaseTest{
         String price = mobileSteps.getMobilePrice(mobile.getHybridApp());
         assertEquals(price, mobile.getHybridAppPrice());
     }
+
+    @Test
+    public void verifyMobileAppPrice1(){
+        String price = mobileSteps.getMobilePrice(mobile.getMobileApp());
+        assertEquals(price, mobile.getMobileAppPrice());
+    }
+
+    @Test
+    public void verifyHybridAppPrice1(){
+        String price = mobileSteps.getMobilePrice(mobile.getHybridApp());
+        assertEquals(price, mobile.getHybridAppPrice());
+    }
+
+    @Test
+    public void verifyMobileAppPrice2(){
+        String price = mobileSteps.getMobilePrice(mobile.getMobileApp());
+        assertEquals(price, mobile.getMobileAppPrice());
+    }
+
+    @Test
+    public void verifyHybridAppPrice2(){
+        String price = mobileSteps.getMobilePrice(mobile.getHybridApp());
+        assertEquals(price, mobile.getHybridAppPrice());
+    }
+
+    @Test
+    public void verifyMobileAppPrice3(){
+        String price = mobileSteps.getMobilePrice(mobile.getMobileApp());
+        assertEquals(price, mobile.getMobileAppPrice());
+    }
+
+    @Test
+    public void verifyHybridAppPrice3(){
+        String price = mobileSteps.getMobilePrice(mobile.getHybridApp());
+        assertEquals(price, mobile.getHybridAppPrice());
+    }
+
+    @Test
+    public void verifyMobileAppPrice4(){
+        String price = mobileSteps.getMobilePrice(mobile.getMobileApp());
+        assertEquals(price, mobile.getMobileAppPrice());
+    }
+
+    @Test
+    public void verifyHybridAppPrice4(){
+        String price = mobileSteps.getMobilePrice(mobile.getHybridApp());
+        assertEquals(price, mobile.getHybridAppPrice());
+    }
+
+    @Test
+    public void verifyMobileAppPrice5(){
+        String price = mobileSteps.getMobilePrice(mobile.getMobileApp());
+        assertEquals(price, mobile.getMobileAppPrice());
+    }
+
+    @Test
+    public void verifyHybridAppPrice5(){
+        String price = mobileSteps.getMobilePrice(mobile.getHybridApp());
+        assertEquals(price, mobile.getHybridAppPrice());
+    }
+
+    @Test
+    public void verifyMobileAppPrice6(){
+        String price = mobileSteps.getMobilePrice(mobile.getMobileApp());
+        assertEquals(price, mobile.getMobileAppPrice());
+    }
+
+    @Test
+    public void verifyHybridAppPrice6(){
+        String price = mobileSteps.getMobilePrice(mobile.getHybridApp());
+        assertEquals(price, mobile.getHybridAppPrice());
+    }
+
+    @Test
+    public void verifyMobileAppPrice7(){
+        String price = mobileSteps.getMobilePrice(mobile.getMobileApp());
+        assertEquals(price, mobile.getMobileAppPrice());
+    }
+
+    @Test
+    public void verifyHybridAppPrice7(){
+        String price = mobileSteps.getMobilePrice(mobile.getHybridApp());
+        assertEquals(price, mobile.getHybridAppPrice());
+    }
+
+    @Test
+    public void verifyMobileAppPrice8(){
+        String price = mobileSteps.getMobilePrice(mobile.getMobileApp());
+        assertEquals(price, mobile.getMobileAppPrice());
+    }
+
+    @Test
+    public void verifyHybridAppPrice8(){
+        String price = mobileSteps.getMobilePrice(mobile.getHybridApp());
+        assertEquals(price, mobile.getHybridAppPrice());
+    }
+
+    @Test
+    public void verifyMobileAppPrice9(){
+        String price = mobileSteps.getMobilePrice(mobile.getMobileApp());
+        assertEquals(price, mobile.getMobileAppPrice());
+    }
+
+    @Test
+    public void verifyHybridAppPrice9(){
+        String price = mobileSteps.getMobilePrice(mobile.getHybridApp());
+        assertEquals(price, mobile.getHybridAppPrice());
+    }
+
+    @Test
+    public void verifyMobileAppPrice10(){
+        String price = mobileSteps.getMobilePrice(mobile.getMobileApp());
+        assertEquals(price, mobile.getMobileAppPrice());
+    }
+
+    @Test
+    public void verifyHybridAppPrice10(){
+        String price = mobileSteps.getMobilePrice(mobile.getHybridApp());
+        assertEquals(price, mobile.getHybridAppPrice());
+    }
 }
